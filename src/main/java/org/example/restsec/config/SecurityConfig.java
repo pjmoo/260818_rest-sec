1c1842b295a2b4489bccaa0c1346691f24577b60
pjmoo <00pjmoooo@gmail.com>
2026-08-18 11:48:52 +0900

    feat: 정적 index.html 추가 및 SecurityConfig 허용 경로 추가

--- src/main/java/org/example/restsec/config/SecurityConfig.java
+++ src/main/java/org/example/restsec/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers(
+                                        "/", "/index.html",
                                         "/swagger-ui/**",
                                         "/swagger-ui.html",
                                         "/v3/api-docs/**")

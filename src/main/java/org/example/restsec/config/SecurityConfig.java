ed61e4454deb31db8f79d703978f5b33eb0c3c41
pjmoo <00pjmoooo@gmail.com>
2026-08-18 10:18:42 +0900

    feat: JPA Auditing, Security 설정 및 ChairEntity 추가

---
+++ src/main/java/org/example/restsec/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package org.example.restsec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    // SecurityFilterChain <- 기본값으로 알아서 설정되는 것 대신
+    // 내 커스텀으로 넣어주겠다
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        // 이 사이에 http에다가 메서드 체이닝 -> 설정을 주입
+        return http
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers(
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html",
+                                        "/v3/api-docs/**").permitAll()
+                )
+                .build();
+    }
+}
